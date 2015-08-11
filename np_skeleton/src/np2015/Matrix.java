169e037cb95ef7d6303e7547cb9904d5d2e62558
s9ev.paul@stud.uni-saarland.de <s9evpaul@stud.uni-saarland.de>
2015-08-11 13:43:53 +0200

    iterationen in spalten, amtrix

--- np_skeleton/src/np2015/Matrix.java
+++ np_skeleton/src/np2015/Matrix.java
@@ -5,12 +5,13 @@ import java.util.ArrayList;
 
 
 // verwaltet alle Spalten
-public class Matrix {
+public class Matrix extends Thread{
 	
 	private ArrayList<Spalten> alleSpalten = new ArrayList<Spalten>();
 	private GraphInfo ginfo = new GraphInfo(0, 0);
 	public static int height;
 	public static int width;
+	public int iterationen = 100;
 	
 	//Konstruktor
 	//TODO: macht es sinn darauf zu bestehen das der initiale Knoten mit dem Konstruktor eingeführt wird?
@@ -19,6 +20,15 @@ public class Matrix {
 		this.height = ginfo.height;
 		this.width = ginfo.width;
 	}
+	
+	public void startSpalten(){
+		Thread threadSpalten = new Thread();
+		for(int i = 0; i < width; i++){
+			Spalten spalte = new Spalten(i, iterationen);
+
+		}
+	}
+	
 	
 	public void addSpalte(Spalten p){
 		ArrayList<Spalten> alleSpalten = getAlleSpalten();
