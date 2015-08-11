3d2f79b23c4e06a0eb6bd783740ae091ffdb7306
s9ev.paul@stud.uni-saarland.de <s9evpaul@stud.uni-saarland.de>
2015-08-11 11:37:06 +0200

    Seq berechnung abfrage

--- np_skeleton/src/np2015/Seq_Berechnung.java
+++ np_skeleton/src/np2015/Seq_Berechnung.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 
 
-public class Seq_Berechnung {
+
+public class Seq_Berechnung{
 
 	/**
 	 *sequenzielle Berechnung nach Vorlage aus der Projektbeschreibung!
@@ -29,6 +30,7 @@ public class Seq_Berechnung {
 					Knoten neighbor_unten= actual.get_neighbor_unten(allNodes);
 					Knoten neighbor_rechts = actual.get_neighbor_rechts(allNodes);
 					Knoten neighbor_links = actual.get_neighbor_links(allNodes);
+					System.out.println("oben "+neighbor_oben + " unten "+ neighbor_unten + " rechts " + neighbor_rechts + " links "+neighbor_links);
 					
 					// top
 					if (neighbor_oben != null){
@@ -39,15 +41,19 @@ public class Seq_Berechnung {
 							ginfo.getRateForTarget(actual.getX(), actual.getY(), Neighbor.Top));
 					}
 					// down
+					if (neighbor_unten != null){
 					neighbor_unten.setAkku(neighbor_unten.getAkku() + actual.getCurrent_value()*
 							ginfo.getRateForTarget(actual.getX(), actual.getY(), Neighbor.Bottom));
 					actual.setAkku(actual.getAkku()-actual.getCurrent_value()*
 							ginfo.getRateForTarget(actual.getX(), actual.getY(), Neighbor.Bottom));
+					}
 					// right
+					if (neighbor_rechts != null){
 					neighbor_rechts.setAkku(neighbor_rechts.getAkku() + actual.getCurrent_value()*
 							ginfo.getRateForTarget(actual.getX(), actual.getY(), Neighbor.Right));
 					actual.setAkku(actual.getAkku()-actual.getCurrent_value()*
 							ginfo.getRateForTarget(actual.getX(), actual.getY(), Neighbor.Right));
+					}
 					//left
 					if (neighbor_links != null){
 					neighbor_links.setAkku(neighbor_links.getAkku() + actual.getCurrent_value()*
